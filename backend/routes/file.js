e34254c483cc75d1763815d2dbbc151ebfdab263
pawareliza14 <pawareliza14@gmail.com>
2025-10-19 22:30:45 +0530

    testing

--- backend/routes/file.js
+++ backend/routes/file.js
@@ -7,8 +7,9 @@ const path = require('path');
 
 // Configure Multer for file uploads
 const storage = multer.diskStorage({
+
   destination: (req, file, cb) => {
-    cb(null, 'uploads/');
+    cb(null, '/tmp');
   },
   filename: (req, file, cb) => {
     const fileExt = path.extname(file.originalname);
